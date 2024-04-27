aee53107f6cc58c4f12aae59602fa4b946238f21
fesantospalmeira <felipesantos7938@gmail.com>
2024-04-26 23:28:44 -0300

    Implementation back-end connection

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 
 
 const AppContainer = styled.div`
-
     width: 100vw;
     height: 100vh;
     background-image: linear-gradient(90deg, #1f5f61 55%, #0b8185);
